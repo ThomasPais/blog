164c6fa3f88a5de14836848bfdca22e68759847d
Thomas Païs <thomas.pais@outlook.com>
2023-11-23 19:43:33 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Heart from '~icons/mdi/heart';
+</script>
+
+<h1>Welcome to SvelteKit <Heart /></h1>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
